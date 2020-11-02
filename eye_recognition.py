5ebd904bbabbd50fa1e9b109224a8cc2877b3ea9
andre <andrerxav@gmail.com>
2020-11-02 02:23:25 -0300

    version 1.0

--- eye_recognition.py
+++ eye_recognition.py
@@ -7,13 +7,12 @@ def redim(img, largura):
     img = cv2.resize(img, (largura, alt), interpolation = cv2.INTER_AREA)
     return img
 
-
 df = cv2.CascadeClassifier('haarcascade_eye.xml')
+img_path = 'C:\\Users\\andre\\Desktop\\Face_Recognition\\images\\img'
+aux = []
+t_end = time.time() + 60*0.5  # 60 * minutes program will run
 
-camera = cv2.VideoCapture(0)
-
-test1 = []
-t_end = time.time() + 60*0.5
+camera = cv2.VideoCapture(0)  #Opens WebCam
 
 while time.time()<t_end:
     (sucesso, frame) = camera.read()
@@ -25,24 +24,22 @@ while time.time()<t_end:
     faces = df.detectMultiScale(frame_pb, scaleFactor = 1.3, minNeighbors=3)
     frame_temp = frame.copy()
 
-    if faces != []:
-        test = cv2.resize(frame_pb, (28, 28), interpolation = cv2.INTER_AREA)
-        resize = test.flatten().reshape((1, 784))
-       
-        test1.append(resize)
-
-
-
+    if faces != []:   #if an eye is recognized 
+        #test = cv2.resize(frame_pb, (28, 28), interpolation = cv2.INTER_AREA)  # makes the image into 784 pixels, just with wanted converted and not images
+        #resize = test.flatten().reshape((1, 784))   # making the image into an array
+        aux.append(frame)
 
     for (x, y, lar, alt) in faces:
-        cv2.rectangle(frame_temp, (x, y), (x + lar, y + alt), (0, 255, 0), 2)
+        cv2.rectangle(frame_temp, (x, y), (x + lar, y + alt), (0, 255, 0), 2)   # make the rectangle in the eye
     
     cv2.imshow("Encontrando faces...", redim(frame_temp, 640))
     
     if cv2.waitKey(1) & 0xFF == ord("s"):
         break
 
-
-print(test1[0][0])
 camera.release()
 cv2.destroyAllWindows()
+cont = 0
+for img in aux:
+    cv2.imwrite(img_path +str(cont)+'.jpg',img)
+    cont+=1
